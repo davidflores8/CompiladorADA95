0e5015430853d838e0e98ba60d00a215ea11220a
davidflores8 <davidflores126@unitec.edu>
2021-08-30 23:20:58 -0600

    Flex y Cup implementados

---
+++ src/compilador_ada95/main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador_ada95;
+
+import java.io.FileReader;
+
+/**
+ *
+ * @author David
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Main");
+        
+        try{
+            Yylex lexer  = new Yylex( new FileReader("src/compilador_ada95/entrada.txt")); 
+            
+            parser p = new parser(lexer);
+            
+            p.parse();
+            
+            
+        }catch(Exception e){
+                e.printStackTrace();
+        }
+    }
+    
+}
