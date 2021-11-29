f1827fbe8360836081c0b33ea0a66d0bd6061aaa
Daniel Agurcia <danielagurcia90@gmail.com>
2021-11-29 02:21:25 -0600

    Estructura Cuadruplos

---
+++ src/compilador_ada95/Cuadruplo.java
@@ -0,0 +1,62 @@
+package compilador_ada95;
+import java.util.ArrayList;
+
+public class Cuadruplo {
+
+    private ArrayList<FilaCuadruplo> tablaCuadruplos;
+
+    Cuadruplo() {
+        this.tablaCuadruplos = new ArrayList<>();
+    }
+
+    public ArrayList<FilaCuadruplo> getListaCuadruplo(){
+        return tablaCuadruplos;
+    }
+
+    public void addCuadruplo(String operacion, String arg1, String arg2, String destino) {
+        tablaCuadruplos.add(new FilaCuadruplo(operacion, arg1, arg2, destino));
+    }
+
+    public void addCuadruplo(String operacion, String arg1, String destino) {
+        tablaCuadruplos.add(new FilaCuadruplo(operacion, arg1, destino));
+    }
+
+    public void addCuadruplo(String operacion, String arg1) {
+        tablaCuadruplos.add(new FilaCuadruplo(operacion, arg1));
+    }
+
+    public void addCuadruplo(String operacion, String arg1, int lugar) {
+        tablaCuadruplos.add(lugar,new FilaCuadruplo(operacion, arg1));
+    }
+
+    public void imprimirCuadruplos() {
+        int i = 0;
+        for (FilaCuadruplo fila : this.tablaCuadruplos) {
+            System.out.println(i + ": " + fila.toString());
+            i++;
+        }
+    }
+
+    public int siguienteCuad(){
+        return this.tablaCuadruplos.size();
+    }
+
+    public void completa(ArrayList<Integer> lista, int indice){
+        if (lista != null){
+            for(int i = 0; i < lista.size(); i++){
+                System.out.println((tablaCuadruplos.get(lista.get(i))));
+                if (tablaCuadruplos.get(lista.get(i)).hayArg1()){
+                    if (!tablaCuadruplos.get(lista.get(i)).getArg1().equals(Integer.toString(indice))){
+                        tablaCuadruplos.get(lista.get(i)).setDestino(Integer.toString(indice));
+                    }                    
+                } else {
+                    tablaCuadruplos.get(lista.get(i)).setArg1(Integer.toString(indice));
+                }                
+            }
+        }        
+    }
+
+    public void eliminarUltimo(){
+        tablaCuadruplos.remove(tablaCuadruplos.size() - 1);
+    }
+}
---
+++ src/compilador_ada95/FilaCuadruplo.java
@@ -0,0 +1,84 @@
+package compilador_ada95;
+
+public class FilaCuadruplo{
+
+    private String operacion;
+    private String arg1;
+    private String arg2;
+    private String destino;
+
+    FilaCuadruplo(String _operacion, String _arg1, String _arg2, String _destino) {
+        this.operacion = _operacion;
+        this.arg1 = _arg1;
+        this.arg2 = _arg2;
+        this.destino = _destino;
+    }
+
+    FilaCuadruplo(String _operacion, String _arg1, String _destino) {
+        this.operacion = _operacion;
+        this.arg1 = _arg1;
+        this.arg2 = "";
+        this.destino = _destino;
+    }
+
+    FilaCuadruplo(String _operacion, String _arg1) {
+        this.operacion = _operacion;
+        this.arg1 = _arg1;
+        this.arg2 = "";
+        this.destino = "";
+    }
+
+    //setters & getters
+
+    public void setArg1(String _arg1){
+        this.arg1 = _arg1;
+    }
+
+    public String getArg1(){
+        return this.arg1;
+    }
+
+    public String getArg2(){
+        return this.arg2;
+    }
+
+    public void setArg2(String _arg2){
+        this.arg2 = _arg2;
+    }
+
+    public String getOp(){
+        return this.operacion;
+    }
+
+    public void setOp(String _operacion){
+        this.operacion = _operacion;
+    }
+
+    public String getDestino(){
+        return this.destino;
+    }
+
+    public void setDestino(String _destino){
+        this.destino = _destino;
+    }
+
+    //otros metodos
+
+    public boolean hayArg1(){
+        if (arg1.equals("")){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String toString() {
+        if (this.arg2.equals("") && this.destino.equals("")){
+            return this.operacion + " " + this.arg1;
+        }
+        if (this.arg2.equals("")) {
+            return this.operacion + " " + this.arg1 + " " + this.destino;
+        }        
+        return this.operacion + " " + this.arg1 + " " + this.arg2 + " " + this.destino;
+    }
+}
