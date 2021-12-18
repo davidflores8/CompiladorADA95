ab807d561e37e1e03b689e1d42a6528c982aef3f
Daniel Agurcia <danielagurcia90@gmail.com>
2021-12-18 17:44:55 -0600

    Codigo Final

--- Pruebas/Prueba4.adb
+++ Pruebas/Prueba4.adb
@@ -1,17 +1,10 @@
 procedure Hola (Valor: in Integer;  Valor2: out Float) is
-    primero,segundo:Integer:=34;
-    principal,secundario:Float:=34.45;
-begin	
-put("A continuacion viene una funcion");
-
-	function mayor (Primero: in Integer; Segundo: in Integer) return Integer is 
-    	final:Integer;
-    begin   
-            if(Primero>Segundo then 
-                Final:=Primero;
-            else
-                Final:=Segundo;
-            end if;
-    return Final;
-    end mayor;
-end ; 
+    primero,segundo,Final:Integer:=34;
+    begin	
+        put("A continuacion viene una funcion");
+        if(Primero>Segundo) then 
+            Final:=Primero;
+        else
+            Final:=Segundo;
+        end if;
+end Hola; 
